a5e8261e583756a527f8e59b5ba53c01ae499f2f
Juan <juanfelipecardenas4@gmail.com>
2023-08-10 18:59:58 -0400

    Fixed paddings.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import WorkoutGallery from "../../components/workoutGallery";
 import useStore from "../../store/useStore";
 import { useEffect } from "react";
+import "./styles.scss";
 const HomePage = () => {
   const { fetchPlans, plans } = useStore((state) => state);
 
@@ -9,8 +10,8 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
-      <h2>My Workouts</h2>
+    <div className="workouts-page">
+      <h2 className="workouts-page__heading">My Workouts</h2>
       <WorkoutGallery workouts={plans.data} />
     </div>
   );
